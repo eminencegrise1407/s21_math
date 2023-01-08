8e0ad03ebe4abd818891bd3804c3d5be0fc532f7
Danil Vasilenko <47531105+eminencegrise1407@users.noreply.github.com>
2023-01-08 10:46:06 +0300

    Update s21_exp.c

--- s21_exp.c
+++ s21_exp.c
@@ -2,14 +2,25 @@
 #include <math.h>
 
 long double s21_exp(double x) {
-  long double result;
+  long double result = 0;
   long double n;
   int i = 0;
-  do {
-    n = pow(x, i) / s21_factorial(i);
-    result += n;
-    i++;
-  } while (n > 1e-16);
+  if (isnan(x)) {
+    result = s21_NAN;
+  } 
+  if (isinf(x)) {
+    if (x < 0){
+        result = 0;
+    } else {
+        result = s21_INFINITY;
+    }
+  } else {
+    do {
+      n = pow(x, i) / s21_factorial(i);
+      result += n;
+      i++;
+    } while (n > 1e-16);
+  }
   // добавить проверки на NAN и бесконечность,  заменить 1е-16, заменить pow
   // и потестить еще, а то мало тестил
   return result;
