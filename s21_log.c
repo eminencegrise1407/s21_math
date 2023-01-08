498c9867c3c13e4fe64a5c87833790ce9758aaf1
Danil Vasilenko <47531105+eminencegrise1407@users.noreply.github.com>
2023-01-08 19:15:23 +0300

    Update s21_log.c

--- s21_log.c
+++ s21_log.c
@@ -4,16 +4,25 @@ long double s21_log(double x) {
     double result = 0;
     double z, y;
     double power = 1.0;
-
-    z = (x + 1) / (x - 1);
-    double step = ((x - 1) * (x - 1)) / ((x + 1) * (x + 1));
-    while (z > 1e-16)
-    {
-        z *= step;
-        y = (1 / power) * z;
-        result = result + y;
-        power = power + 2;
+    if (x < 0) {
+        result = s21_NAN;
+    }
+    if (x == 0) {
+        result = -s21_INFINITY;
+    }
+    if (x == 1) {
+        result = 0;
+    }
+    if (is_nan(x)) {
+        result = s21_NAN;
+    }
+    if (is_inf(x)) {
+        result = s21_INFINITY;
+    }
+    if (x > 0 && x != 1) {
+        double k = 4 / (x * s21_pow(2, 16));
+        result = s21_PI / (2 * s21_agm(1, k)) - 16 * 0.69314718056;
     }
     // добавить проверку на x < 0 и прочую лабуду, заменить 1e-16
-    return 2 * result;
+    return result;
 }
