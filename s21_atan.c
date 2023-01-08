0cc63f8f856c16e99d4314943c4567d6ba81c31c
Danil Vasilenko <47531105+eminencegrise1407@users.noreply.github.com>
2023-01-08 10:34:51 +0300

    Update s21_atan.c

--- s21_atan.c
+++ s21_atan.c
@@ -1,23 +1,25 @@
 #include "s21_math.h"
 
 long double s21_atan(double x) {
-    long double res;
+    long double res = s21_NAN;
     long double n;
     int i = 0;
     int sign = 1;
-    if (fabs(x) < 1) {
-        do {
-            n = pow(x, 2 * i + 1) / (2 * i + 1);
-            res += sign * n;
-            i++;
-            sign *= -1;
-        } while (fabs(n) > 1e-16);
-    }
-    if (x > 1) {
-        res = s21_PI/2 - s21_atan(1/x);
-    }
-    if (x < -1) {
-        res = -s21_PI/2 - s21_atan(1/x);
+    if (!is_nan(x)) {
+        if (fabs(x) < 1) {
+            do {
+                n = pow(x, 2 * i + 1) / (2 * i + 1);
+                res += sign * n;
+                i++;
+                sign *= -1;
+            } while (fabs(n) > 1e-16);
+        }
+        if (x > 1) {
+            res = s21_PI/2 - s21_atan(1/x);
+        }
+        if (x < -1) {
+            res = -s21_PI/2 - s21_atan(1/x);
+        }
     }
     // добавить проверки на NAN и бесконечность,  заменить 1е-16, заменить pow
     return res;
