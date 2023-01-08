d0015a8dc81791f031031a5aa5cb62573ba38c9d
Danil Vasilenko <47531105+eminencegrise1407@users.noreply.github.com>
2023-01-08 10:28:55 +0300

    Update s21_acos.c

--- s21_acos.c
+++ s21_acos.c
@@ -1,9 +1,11 @@
 #include "s21_math.h"
 
 long double s21_acos(double x) {
-    long double result = 0.0;
-    long double y = x / pow((1 - x * x), 0.5);
-    result = s21_PI / 2 - s21_atan(y);
+    long double result = s21_NAN;
+    if (-1 <= x && x <= 1) {
+        long double y = x / pow((1 - x * x), 0.5);
+        result = s21_PI / 2 - s21_atan(y);
+    }
     // заменить pow, добавить проверки fabs(x) < 1 и прочую лабуду
     return result;
 }
