b59395f519977ed7b01225e0d16f858f091a4081
Danil Vasilenko <47531105+eminencegrise1407@users.noreply.github.com>
2023-01-08 10:19:37 +0300

    Update s21_fmod.c

--- s21_fmod.c
+++ s21_fmod.c
@@ -1,5 +1,18 @@
 #include "s21_math.h"
 
 long double s21_fmod(double x, double y) {
-    
+  long double res = 0.0;
+  int sign = 1;
+  y = s21_fabs(y);
+  if (x < 0) {
+    sign = -1;
+    x = s21_fabs(x);
+  }
+  if (y == 0) {
+    res = s21_NAN;
+  } else {
+    long long int n = x / y;
+    res = (long double)x - n * (long double)y;
+  }
+  return res * sign;
 }
