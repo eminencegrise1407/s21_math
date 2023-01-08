27c67b249603af3ecf791fcfa270600c1068c8e6
Danil Vasilenko <47531105+eminencegrise1407@users.noreply.github.com>
2023-01-08 10:29:28 +0300

    Update s21_asin.c

--- s21_asin.c
+++ s21_asin.c
@@ -1,9 +1,11 @@
 #include "s21_math.h"
 
 long double s21_asin(double x) {
-    long double result = 0.0;
-    long double y = x / pow((1 - x * x), 0.5);
-    result = s21_atan(y);
+    long double result = s21_NAN;
+    if (-1 <= x && x <= 1) {
+        long double y = x / pow((1 - x * x), 0.5);
+        result = s21_atan(y);
+    }
     // заменить pow, добавить проверки fabs(x) < 1 и прочую лабуду
     return result;
 }
